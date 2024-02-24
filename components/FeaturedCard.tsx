4124665e6679712b7825967407ba8b6afc1085fd
Emile Tremblay <emiletremblay01@gmail.com>
2024-02-23 19:08:26 -0500

    Add useIsMobile hook to FeaturedCard component

--- components/FeaturedCard.tsx
+++ components/FeaturedCard.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import Image, { StaticImageData } from "next/image";
 import { ArrowUpRight } from "lucide-react";
 import Tilt from "react-next-tilt";
+import useIsMobile from "@/hooks/useIsMobile";
 export const FeaturedCard = ({
   className,
   name,
@@ -18,35 +19,63 @@ export const FeaturedCard = ({
   imgAlt: string;
   color: string;
 }) => {
+  const isMobile = useIsMobile();
+
   return (
-    <Tilt
-      tiltMaxAngleX={6}
-      tiltMaxAngleY={6}
-      lineGlareEnable={false}
-      spotGlareEnable={false}
-      className=""
-    >
-      <div
-        className={cn(
-          "group flex h-full flex-col overflow-hidden rounded-2xl border border-white border-opacity-60  shadow  backdrop-blur",
-          className,
-        )}
-      >
-        <div className={cn("h-40", color)}>
-          <div className="flex items-center gap-1 px-4 py-3 ">
-            <h3 className="font-bold duration-200">{name}</h3>
-            <ArrowUpRight className="aspect-square h-4  transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 md:h-5" />
+    <div>
+      {isMobile ? (
+        <div
+          className={cn(
+            "group flex h-full flex-col overflow-hidden rounded-2xl border border-white border-opacity-60 shadow backdrop-blur",
+            className,
+          )}
+        >
+          <div className={cn("h-40", color)}>
+            <div className="flex items-center gap-1 px-4 py-3">
+              <h3 className="font-bold duration-200">{name}</h3>
+              <ArrowUpRight className="aspect-square h-4 transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 md:h-5" />
+            </div>
           </div>
+          <div className="h-fit w-full border-t border-inherit border-opacity-60 transition-opacity duration-200 group-hover:opacity-0">
+            <p className="px-4 py-3 text-xs">{description}</p>
+          </div>
+          <Image
+            src={imgUrl}
+            alt={imgAlt}
+            className="pointer-events-none absolute -z-10 h-full w-full object-cover opacity-20 blur-sm transition duration-200 ease-in-out group-hover:scale-105 group-hover:opacity-80 group-hover:blur-none"
+          />
         </div>
-        <div className=" h-fit w-full border-t border-inherit border-opacity-60   transition-opacity duration-200 group-hover:opacity-0 ">
-          <p className="px-4 py-3 text-xs  ">{description}</p>
-        </div>
-        <Image
-          src={imgUrl}
-          alt={imgAlt}
-          className="pointer-events-none absolute -z-10 h-full w-full object-cover opacity-20 blur-sm transition duration-200 ease-in-out group-hover:scale-105 group-hover:opacity-80 group-hover:blur-none"
-        />
-      </div>
-    </Tilt>
+      ) : (
+        <Tilt
+          tiltMaxAngleX={6}
+          tiltMaxAngleY={6}
+          lineGlareEnable={false}
+          spotGlareEnable={false}
+          className=""
+        >
+          <div
+            className={cn(
+              "group flex h-full flex-col overflow-hidden rounded-2xl border border-white border-opacity-60 shadow backdrop-blur",
+              className,
+            )}
+          >
+            <div className={cn("h-40", color)}>
+              <div className="flex items-center gap-1 px-4 py-3">
+                <h3 className="font-bold duration-200">{name}</h3>
+                <ArrowUpRight className="aspect-square h-4 transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 md:h-5" />
+              </div>
+            </div>
+            <div className="h-fit w-full border-t border-inherit border-opacity-60 transition-opacity duration-200 group-hover:opacity-0">
+              <p className="px-4 py-3 text-xs">{description}</p>
+            </div>
+            <Image
+              src={imgUrl}
+              alt={imgAlt}
+              className="pointer-events-none absolute -z-10 h-full w-full object-cover opacity-20 blur-sm transition duration-200 ease-in-out group-hover:scale-105 group-hover:opacity-80 group-hover:blur-none"
+            />
+          </div>
+        </Tilt>
+      )}
+    </div>
   );
 };
