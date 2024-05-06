2663679ccc21bc08b576d7c0268dd0be8b3849cd
Emile Tremblay <emiletremblay01@gmail.com>
2024-05-05 22:11:39 -0400

    added util function getErrorMessage

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,17 @@ export function enableScroll(isScrollable: boolean) {
   if (!isScrollable) document.body.style.overflow = "hidden";
   else document.body.style.overflow = "";
 }
+
+export function getErrorMessage(error: unknown): string {
+  let message: string;
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "An unknown error occurred";
+  }
+  return message;
+}
